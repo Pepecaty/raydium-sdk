2262d1a07ae6d3fe2d2eb3c64fc1cff9376d8337
Pepecaty <166151416+Pepecaty@users.noreply.github.com>
2024-07-07 14:54:16 +0400

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,22 @@
-// src/index.d.ts
 import { Connection, PublicKey } from '@solana/web3.js';
+import { RaydiumSDK } from '@raydium-io/raydium-sdk-v2';
 
 export class MyRaydiumWrapper {
   constructor(connection: Connection);
-  getPoolInfo(poolAddress: PublicKey): Promise<any>; // Replace 'any' with the correct type
-  // ... other methods 
+  getPoolInfo(poolAddress: PublicKey): Promise<PoolInfo>; 
+}
+
+interface PoolInfo {
+  tokenA: {
+    mint: PublicKey; 
+    symbol: string; 
+    decimals: number;
+  };
+  tokenB: {
+    mint: PublicKey; 
+    symbol: string;  
+    decimals: number; 
+  };
+  liquidity: BN;   
+
 }
